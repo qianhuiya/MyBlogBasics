ee6852b61ef7584eb1c7280b1b400038c792989d
qianhuiya <2867463524@qq.com>
2020-12-23 16:43:51 +0800

    更新句子

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import 'font-awesome/css/font-awesome.css';
 import "../node_modules/@mdi/font/css/materialdesignicons.min.css"
-import "./assets/basics-js/animate"
+import "./assets/basics/animate"
 import "./assets/iconfont/iconfont.css"
 import axios from 'axios'
 import utils from "./utils/index"
 import VueAxios from 'vue-axios'
-import config from './assets/basics-js/config.js'
+import config from './assets/basics/config.js'
 
 Vue.prototype.$config = config;
 Vue.use(VueAxios, axios);
